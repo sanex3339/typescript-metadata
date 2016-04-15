9ae1dc465332d731d49fd3d7758ddc4da2badf05
kachalov <t.kachalov@advmaker.net>
2016-04-15 11:33:48 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function __metadata(k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+if (typeof module !== 'undefined' && module.exports) {
+    exports = module.exports = __metadata;
+}
+
+exports.__metadata = __metadata;
